7702e1e9109f80a8c1c01d1f7d03956e8543a6f6
Justin-Perumal <1452891@students.wits.ac.za>
2021-03-31 22:32:38 +0200

    Project and Build
    Project and Build pushed to github

---
+++ Communication Design prototype/Assets/Scripts/Unit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Unit : MonoBehaviour
+{
+    public Sprite[] CharacterSprites;
+    public string UnitName;
+    public float Damage;
+    public float MaxHP;
+    public float CurrentHP; 
+    public float HealAmount;
+}
